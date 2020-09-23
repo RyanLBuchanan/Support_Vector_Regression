e386538eac2c40b030388a5d04be2215f81b772f
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-09-22 18:05:05 -0600

    Import dataset

--- Support_Vector_Regression.py
+++ Support_Vector_Regression.py
@@ -1,2 +1,31 @@
 # Support Vector Regression tutorial from Machine Learning A-Z - SuperDataScience
 # Input by Ryan L Buchanan 18SEP20
+
+# Import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Import Dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+print(X)
+
+print(y)
+
+# Feature Scaling
+
+# Train the SVR model on the whole dataset
+
+
+# Predict a new result
+
+
+# Visualize the SVR results
+
+
+# Visualize the SVR results (for higher resolution and smooth curve)
+
+
